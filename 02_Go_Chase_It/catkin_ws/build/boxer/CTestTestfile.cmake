239e96bbee9cb45141a664fd70f7e831e5acb530
Samuel-Biokpo Asimiea <biokposamuel.asimiea@ontariotechu.net>
2021-07-20 11:11:32 -0400

    deleted old files. redundant.

--- 02_Go_Chase_It/catkin_ws/build/boxer/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/samuel/RSE_Udacity/RSE_Udacity/02_Go_Chase_It/catkin_ws/src/boxer
-# Build directory: /home/samuel/RSE_Udacity/RSE_Udacity/02_Go_Chase_It/catkin_ws/build/boxer
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- 02_Go_Chase_It/catkin_ws/build/gtest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/samuel/RSE_Udacity/RSE_Udacity/02_Go_Chase_It/catkin_ws/build/gtest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
