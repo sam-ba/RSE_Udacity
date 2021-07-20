752fe8a68dfd192d9efd21332263b76a88a61fd9
Samuel-Biokpo Asimiea <biokposamuel.asimiea@ontariotechu.net>
2021-07-20 11:03:28 -0400

    directory clean up

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/samuel/RSE_Udacity/RSE_Udacity/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/samuel/RSE_Udacity/RSE_Udacity/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/samuel/RSE_Udacity/RSE_Udacity/catkin_ws/devel;/opt/ros/melodic' != '' else []
---
+++ catkin_ws/build/my_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/samuel/RSE_Udacity/RSE_Udacity/catkin_ws/src/my_robot
+# Build directory: /home/samuel/RSE_Udacity/RSE_Udacity/catkin_ws/build/my_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
