26170152de37d12363b8b5fa2bb373842aef14ae
Samuel-Biokpo Asimiea <biokposamuel.asimiea@ontariotechu.net>
2021-08-06 12:04:16 -0400

    loop through pixel to find ball and move robot

--- catkin_ws/src/ball_chaser/src/process_image.cpp
+++ catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -2,29 +2,70 @@
 #include "ball_chaser/DriveToTarget.h"
 #include <sensor_msgs/Image.h>
 
+using namespace std;
+
 // Define a global client that can request services
 ros::ServiceClient client;
 
 // This function calls the command_robot service to drive the robot in the specified direction
 void drive_robot(float lin_x, float ang_z)
 {
+    ROS_INFO_STREAM("Driving the robot");
+
     // TODO: Request a service and pass the velocities to it to drive the robot
     ball_chaser::DriveToTarget srv;
 
     srv.request.linear_x = lin_x;
     srv.request.angular_z = ang_z;
+
+    //Call the command_robot service and pass the requested motor velocities
+    if(!client.call(srv)){
+        ROS_ERROR("Failed to call service command_robot");
+    }
 }
 
 // This callback function continuously executes and reads the image data
 void process_image_callback(const sensor_msgs::Image img)
 {
-
     int white_pixel = 255;
+    
+    bool ball_detected = false;
+    int step = -1;
 
     // TODO: Loop through each pixel in the image and check if there's a bright white one
+    for(int i = 0; i < (img.height * img.step); i++){
+        if(img.data[i] == white_pixel &&
+           img.data[i+1] == white_pixel &&
+           img.data[i+2] == white_pixel)
+        {
+                ball_detected = true;
+                step = i % img.step;
+        }
+        else{
+            ball_detected = false;
+        }
+    }
+
     // Then, identify if this pixel falls in the left, mid, or right side of the image
-    // Depending on the white ball position, call the drive_bot function and pass velocities to it
+    if(ball_detected == 1){
+        // Depending on the white ball position, call the drive_bot function and pass velocities to it
+        if(step <= img.step * 0.3 && step >= 0){
+            // if the pixel is in the left area, turn left
+            drive_robot(0.0,0.5);
+        }
+        else if(step > img.step * 0.7 && step <= img.step){
+            // if the pixel is in the right area, turn right
+            drive_robot(0.0,-0.5);
+        }
+        else if(step != -1){
+            // if the pixel is in the middle area, move forward in a straight line
+            drive_robot(0.5,0.0);
+        }
+    }
     // Request a stop when there's no white ball seen by the camera
+    else{
+        drive_robot(0.0,0.0);
+    }
 }
 
 int main(int argc, char** argv)
